f0a592224d7b368e48deca1ef6d9c2fd41c7eb63
Malintha <malinthak2@gmail.com>
2014-07-31 09:14:13 +0530

    FakeServer Test

--- test_fakerServer.js
+++ test_fakerServer.js
@@ -32,7 +32,7 @@ fakeServer.prototype = {
     getLocalPort: function get_LocalPort() {
         return this.httpServer.identity.primaryPort;
     },
-    
+
     prefixHandler: function main_PrefixHandler(request, response) {
         response.processAsync();     
         try {
@@ -54,7 +54,7 @@ fakeServer.prototype = {
                     fakeServer.prototype.putHandler(this,request,response);
                 }
                 else if(request.method == 'GET'){
-                //GET requests to scheduleInboxURL/event.ics
+                    //GET requests to scheduleInboxURL/event.ics
                     fakeServer.prototype.getHandler(this,request,response)
                 }
                 else {
@@ -63,12 +63,11 @@ fakeServer.prototype = {
                     response.finish()
                 }
             }
-
         } catch (e) {
             dump('\n\n#### EEE: ' + e + e.fileName + e.lineNumber + '\n');
         }
-     },
-        
+    },
+
     initPropfind: function initPropfind(scope, request,response) {
         let is = request.bodyInputStream;
         let body = NetUtil.readInputStreamToString(is, is.available(), {
@@ -106,7 +105,7 @@ fakeServer.prototype = {
             dump('\n\n#### EEE: ' + e + e.fileName + e.lineNumber + '\n');
         }
     },
-    
+
     calendarHandler: function calendarHandler(scope,request,response) {
         try {
             if (request.method == "OPTIONS") {
@@ -123,7 +122,7 @@ fakeServer.prototype = {
             dump("\n\n#### EEE: " + e + e.fileName + e.lineNumber +"\n");
         }
     },
-    
+
     principalHandler: function principalHandler(scope,request,response) {
         if (request.method == "PROPFIND") {
             response.setStatusLine(request.httpVersion, 207, "Multi-Status");
@@ -151,7 +150,7 @@ fakeServer.prototype = {
             dump("##If-Schedule-Tag-Match");
             matchheader = request.getHeader("If-Schedule-Tag-Match");
         }
-          //create resource in server calendar
+        //create resource in server calendar
         if(request.method=="PUT" && matchheader=="*" && body){
             let tempServerItem = createEventFromIcalString(body);
             scope.storage.addItem(tempServerItem, {
@@ -173,7 +172,6 @@ fakeServer.prototype = {
             catch(e){
                 dump("\n\n#### EEE: " + e + e.fileName + e.lineNumber +"\n");
             }
-
         }
         //modify request
         else if(request.method=='PUT' && matchheader>0 && body){   
@@ -181,7 +179,7 @@ fakeServer.prototype = {
             let changeItemId = scope.storage.getMetaData(matchheader);
             let newItem = createEventFromIcalString(body);
             let oldItem = null;
-            
+
             scope.storage.getItem(changeItemId, {
                 onGetResult: function (cal, stat, type, detail, count, items) {
                     oldItem = items[0];
@@ -201,11 +199,13 @@ fakeServer.prototype = {
             });
         }
     },
-    
+
     getHandler: function(scope,request,response){
         //get the itemID from request.path
+        let s = request.path.split(scope._propertyBag.scheduleInboxURL).pop();
+        let getItemId = s.split('.ics')[0];
         let tempGetItem = null;
-        scope.storage.getItem(tempServerItem.id, {
+        scope.storage.getItem(getItemId, {
             onGetResult: function (cal, stat, type, detail, count, items) {
                 tempGetItem = items[0];
             },
@@ -215,7 +215,7 @@ fakeServer.prototype = {
         response.write(tempGetItem.icalString);
         response.finish();
     },
-     
+
     getItemString: function(itemId,calendar) {
         //get a icalString for given Item Id
         let tempGetItemString = null;
@@ -227,7 +227,7 @@ fakeServer.prototype = {
         });
         return tempGetItemString;
     },
-    
+
     //Randomly generates a tag with given combination
     generateTag: function generateTag() {
         let tag = "";
@@ -235,19 +235,16 @@ fakeServer.prototype = {
         for(let i=0; i < 5; i++ )
             tag += possible.charAt(Math.floor(Math.random() * possible.length));
         return tag;    
-    },
-
-    test: function test(){
-       dump("\n\n###TESTSTS###");
     }
+
 };
 
-//Example server implementation
-function sogo() {
-    this.id = '1';
+//Example server implementation goes from here
+
+//Create the fake server model
+function exampleServer() {
     this._propertyBag = {
-        name: 'sogo',
-        id: 'calendar1',
+        name: 'example',
         calendarHomeSetset: '/calendar/',
         scheduleInboxURL: '/calendar/xpcshell/',
         scheduleOutboxURL: '/calendar/xpcshell/',
@@ -273,62 +270,61 @@ function sogo() {
     };
 
     this._responseTemplates = {
-                         
-        _initPropfind: '<?xml version="1.0" encoding="UTF-8"?>\n'+
-            '<D:multistatus xmlns:a="urn:ietf:params:xml:ns:caldav" xmlns:b="http://calendarserver.org/ns/" xmlns:D="DAV:">\n' +
-            '   <D:response>\n' +
-            '    <D:href>' + this._propertyBag.scheduleInboxURL + '</D:href>\n' +
-            '     <D:propstat>\n' +
-            '       <D:status>HTTP/1.1 200 OK</D:status>\n' +
-            '       <D:prop>\n' +
-            '         <D:resourcetype>\n' +
-            '           <D:collection/>\n' +
-            '           <calendar xmlns="urn:ietf:params:xml:ns:caldav"/>\n' +
-            '         </D:resourcetype>\n' +
-            '         <D:owner xmlns:D="DAV:">\n' +
-            '           <D:href>' + this._propertyBag.userPrincipalHref + '</D:href>\n' +
-            '         </D:owner>\n' +
-            '         <D:current-user-principal xmlns:D="DAV:">\n' +
-            '           <D:href>' + this._propertyBag.userPrincipalHref + '</D:href>\n' +
-            '         </D:current-user-principal>\n' +
-            '         <n1:supported-calendar-component-set xmlns:n1="urn:ietf:params:xml:ns:caldav" xmlns:D="DAV:">\n' +
-            '            <n1:comp name="' + this._propertyBag.supportedComps[0] + '"/>\n'+
-            '            <n1:comp name="' + this._propertyBag.supportedComps[1] + '"/>\n' +
-            '         </n1:supported-calendar-component-set>\n' +
-            '         <b:getctag>' + this._propertyBag.ctag + '</b:getctag>\n' +
-            '       </D:prop>\n' +
-            '     </D:propstat>\n' +
-            '   </D:response>\n' +
-            ' </D:multistatus>',
-        
-        _principalPropfind: '<?xml version="1.0" encoding="UTF-8"?>\n'+
-            '   <D:multistatus xmlns:a="urn:ietf:params:xml:ns:caldav" xmlns:D="DAV:">\n' +
-            '       <D:response>\n' +
-            '          <D:href>/calendar/xpcshell/</D:href>\n' +
-            '          <D:propstat>\n' +
-            '             <D:status>HTTP/1.1 200 OK</D:status>\n' +
-            '             <D:prop>\n' +
-            '             <a:calendar-home-set>\n' +
-            '                <D:href xmlns:D="DAV:">'+this._propertyBag.calendarHomeSetset+'</D:href>\n' +
-            '             </a:calendar-home-set>\n' +
-            '             <a:schedule-inbox-URL>\n' +
-            '                <D:href xmlns:D="DAV:">'+this._propertyBag.scheduleOutboxURL+'</D:href>\n' +
-            '             </a:schedule-inbox-URL>\n' +
-            '             <a:schedule-outbox-URL>\n' +
-            '                <D:href xmlns:D="DAV:">'+this._propertyBag.scheduleOutboxURL+'</D:href>\n' +
-            '             </a:schedule-outbox-URL>\n' +
-            '             <a:calendar-user-address-set>\n'+
-            '                <D:href xmlns:D="DAV:">mailto:'+this._propertyBag.userAddressSet[0]+'</D:href>\n'+
-            '                <D:href xmlns:D="DAV:">mailto:'+this._propertyBag.userAddressSet[1]+'</D:href>\n'+
-            '             </a:calendar-user-address-set>\n' +
-            '             </D:prop>\n' +
-            '          </D:propstat>\n' +
-            '       </D:response>\n' +
-            '   </D:multistatus>',
-        
 
-};
-    this.refreshResponses = function(){
+        _initPropfind: '<?xml version="1.0" encoding="UTF-8"?>\n'+
+        '<D:multistatus xmlns:a="urn:ietf:params:xml:ns:caldav" xmlns:b="http://calendarserver.org/ns/" xmlns:D="DAV:">\n' +
+        '   <D:response>\n' +
+        '    <D:href>' + this._propertyBag.scheduleInboxURL + '</D:href>\n' +
+        '     <D:propstat>\n' +
+        '       <D:status>HTTP/1.1 200 OK</D:status>\n' +
+        '       <D:prop>\n' +
+        '         <D:resourcetype>\n' +
+        '           <D:collection/>\n' +
+        '           <calendar xmlns="urn:ietf:params:xml:ns:caldav"/>\n' +
+        '         </D:resourcetype>\n' +
+        '         <D:owner xmlns:D="DAV:">\n' +
+        '           <D:href>' + this._propertyBag.userPrincipalHref + '</D:href>\n' +
+        '         </D:owner>\n' +
+        '         <D:current-user-principal xmlns:D="DAV:">\n' +
+        '           <D:href>' + this._propertyBag.userPrincipalHref + '</D:href>\n' +
+        '         </D:current-user-principal>\n' +
+        '         <n1:supported-calendar-component-set xmlns:n1="urn:ietf:params:xml:ns:caldav" xmlns:D="DAV:">\n' +
+        '            <n1:comp name="' + this._propertyBag.supportedComps[0] + '"/>\n'+
+        '            <n1:comp name="' + this._propertyBag.supportedComps[1] + '"/>\n' +
+        '         </n1:supported-calendar-component-set>\n' +
+        '         <b:getctag>' + this._propertyBag.ctag + '</b:getctag>\n' +
+        '       </D:prop>\n' +
+        '     </D:propstat>\n' +
+        '   </D:response>\n' +
+        ' </D:multistatus>',
+
+        _principalPropfind: '<?xml version="1.0" encoding="UTF-8"?>\n'+
+        '   <D:multistatus xmlns:a="urn:ietf:params:xml:ns:caldav" xmlns:D="DAV:">\n' +
+        '       <D:response>\n' +
+        '          <D:href>/calendar/xpcshell/</D:href>\n' +
+        '          <D:propstat>\n' +
+        '             <D:status>HTTP/1.1 200 OK</D:status>\n' +
+        '             <D:prop>\n' +
+        '             <a:calendar-home-set>\n' +
+        '                <D:href xmlns:D="DAV:">'+this._propertyBag.calendarHomeSetset+'</D:href>\n' +
+        '             </a:calendar-home-set>\n' +
+        '             <a:schedule-inbox-URL>\n' +
+        '                <D:href xmlns:D="DAV:">'+this._propertyBag.scheduleOutboxURL+'</D:href>\n' +
+        '             </a:schedule-inbox-URL>\n' +
+        '             <a:schedule-outbox-URL>\n' +
+        '                <D:href xmlns:D="DAV:">'+this._propertyBag.scheduleOutboxURL+'</D:href>\n' +
+        '             </a:schedule-outbox-URL>\n' +
+        '             <a:calendar-user-address-set>\n'+
+        '                <D:href xmlns:D="DAV:">mailto:'+this._propertyBag.userAddressSet[0]+'</D:href>\n'+
+        '                <D:href xmlns:D="DAV:">mailto:'+this._propertyBag.userAddressSet[1]+'</D:href>\n'+
+        '             </a:calendar-user-address-set>\n' +
+        '             </D:prop>\n' +
+        '          </D:propstat>\n' +
+        '       </D:response>\n' +
+        '   </D:multistatus>'     
+    };
+    
+    this.initResponses = function(){
         this._responseTemplates._reportPropfind = '<?xml version="1.0" encoding="UTF-8"?>\n'+
             '   <D:multistatus xmlns:D="DAV:" xmlns:C="urn:ietf:params:xml:ns:caldav">\n'+
             '     <D:response>\n'+
@@ -343,7 +339,7 @@ function sogo() {
             '         </D:propstat>\n'+
             '     </D:response>\n'+
             '   </D:multistatus>\n';
-            
+
         this._responseTemplates._calDataPropfind = '<?xml version="1.0" encoding="UTF-8"?>\n'+
             '   <D:multistatus xmlns:D="DAV:" xmlns:C="urn:ietf:params:xml:ns:caldav">\n'+
             '     <D:response>\n'+
@@ -358,81 +354,61 @@ function sogo() {
             '         </D:propstat>\n'+
             '     </D:response>\n'+
             '   </D:multistatus>\n'
-};
+    };
 }
 
-sogo.prototype = new fakeServer();
+exampleServer.prototype = new fakeServer();
+//create server object using the fake server model
+var exampleServerObj = new exampleServer();
+exampleServer.id = "exampleServer1";
 
-var sogoObj = new sogo();
-sogoObj.id = "Sogo1";
-
+//Implement this to route requests to the server object
 function router(request,response){
-    sogoObj.refreshResponses();
-    sogoObj.prefixHandler(request,response);
+    exampleServerObj.initResponses();
+    exampleServerObj.prefixHandler(request,response);
 }
 
+//Start Client/Server model
 function run_test() {
-   
-    dump("### Server "+sogoObj.id+" started on "+sogoObj.getLocalPort());
-    
-    //create client calendar
+    dump("### Server "+exampleServerObj.id+" started on "+exampleServerObj.getLocalPort());
     do_get_profile();
     registerFakeUMimTyp();
-    
-    do_register_cleanup(() => sogoObj.httpServer.stop(() => {}));
+    do_register_cleanup(() => exampleServerObj.httpServer.stop(() => {}));
     cal.getCalendarManager().startup({onResult: function() {
         run_next_test();
     }});
 }
 
-var client = {
-    icalString :    'BEGIN:VEVENT\n' +
-                    'DTSTART:20140725T230000\n' +
-                    'DTEND:20140726T000000\n' +
-                    'LOCATION:Paris\n'+
-                    'UID: 1b05e158-631a-445f-8c5a-5743b5a05169'+
-                    'TRANSP:OPAQUE\n'+
-                    'ORGANIZER;CN=Organizer Name;SENT-BY="mailto:organizer@example.com":mailto:malinthak2@gmail.com\n'+
-                    'ATTENDEE;CN=Attendee1 Name;PARTSTAT=NEEDS-ACTION;CUTYPE=INDIVIDUAL;ROLE=REQ-PARTICIPANT;X-NUM-GUESTS=0:mailto:attendee1@example.com\n'+
-                    'ATTENDEE;CN=Attendee2 Name;PARTSTAT=NEEDS-ACTION;CUTYPE=INDIVIDUAL;ROLE=REQ-PARTICIPANT;X-NUM-GUESTS=0:mailto:attendee2@example.com\n'+
-                    'END:VEVENT\n',
-    itemId     :    '1b05e158-631a-445f-8c5a-5743b5a05169'
-};
-
 add_task(test_doFakeServer);
 
 function test_doFakeServer(){
-
+    //create client calendar
     let calmgr = cal.getCalendarManager();
-    let calendarURL = 'http://localhost:'+sogoObj.getLocalPort()+sogoObj._propertyBag.scheduleInboxURL;
-    dump('###'+calendarURL);
+    let calendarURL = 'http://localhost:'+exampleServerObj.getLocalPort()+exampleServerObj._propertyBag.scheduleInboxURL;
     let clientCalendar = calmgr.createCalendar("caldav", Services.io.newURI(calendarURL, null, null));
     clientCalendar.name="clientCalendar";
     calmgr.registerCalendar(clientCalendar);
-    dump('registerCalendar');
     yield waitForInit(clientCalendar);
-    dump('waitForInit');
-    var item = createEventFromIcalString(sogoObj._propertyBag.icalString);
+    let item = createEventFromIcalString(exampleServerObj._propertyBag.icalString);
     yield promiseAddItem(item,clientCalendar);
-    dump('promiseAddItem');
+    dump('\n\nAll Done');
 }
 
-
- function registerFakeUMimTyp() {
-  try {
-    Services.dirsvc.get("UMimTyp", Components.interfaces.nsIFile);
-  } catch (e) {
-    Services.dirsvc.registerProvider({
-      getFile: function(prop, persist) {
-        if (prop == "UMimTyp") {
-          var mimeTypes = Services.dirsvc.get("ProfD", Ci.nsIFile);
-          mimeTypes.append("mimeTypes.rdf");
-          return mimeTypes;
-        }
-        throw Components.results.NS_ERROR_FAILURE;
-      }
-    });
-  }
+function registerFakeUMimTyp() {
+    try {
+        Services.dirsvc.get("UMimTyp", Components.interfaces.nsIFile);
+    } catch (e) {
+        Services.dirsvc.registerProvider({
+            getFile: function(prop, persist) {
+                if (prop == "UMimTyp") {
+                    var mimeTypes = Services.dirsvc.get("ProfD", Ci.nsIFile);
+                    mimeTypes.append("mimeTypes.rdf");
+                    return mimeTypes;
+                }
+                throw Components.results.NS_ERROR_FAILURE;
+            }
+        });
+    }
 }
 
 function waitForInit(calendar) {
